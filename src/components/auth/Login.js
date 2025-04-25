b389b00401c0e7ee2625f02d3c855af79c98c061
Bharat-Sarda <bharat.sarda@lumiq.ai>
2025-04-25 11:52:27 +0530

    placeholder added

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -40,7 +40,7 @@ export default function Login() {
                                 <>
                                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900"> Email address</label>
                                     <div className="!mt-2">
-                                        <input id="email" name="email" type="email" value={formik.values.email} onChange={formik.handleChange} autoComplete="off" required
+                                        <input id="email" name="email" type="email" placeholder='Enter your email' value={formik.values.email} onChange={formik.handleChange} autoComplete="off" required
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                         {formik.touched.email && formik.errors.email&& ( <p className="mt-1 text-sm text-red-500">{formik.errors.email}</p>)}
@@ -52,7 +52,7 @@ export default function Login() {
                                         <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900"> Password </label>
                                     </div>
                                     <div className="!mt-2">
-                                        <input id="password" name="password" type="password"  autoComplete="off"  value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} required
+                                        <input id="password" name="password" type="password"  autoComplete="off" placeholder='Enter your password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} required
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                         {formik.touched.password && formik.errors.password && ( <p className="mt-1 text-sm text-red-500">{formik.errors.password}</p>)}
                                     </div>
